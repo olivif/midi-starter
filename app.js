6b51278bb5bfeec99e10d1415f1efabdda914178
Olivia Ifrim <olly2204@yahoo.com>
2016-03-06 00:36:46 +0100

    More cleanup

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 //http://stackoverflow.com/questions/23687635/how-to-stop-audio-in-an-iframe-using-web-audio-api-after-hiding-its-container-di
 var log = console.log.bind(console),
-    keyData = document.getElementById('key_data'),
-    deviceInfoInputs = document.getElementById('inputs'),
     midi;
 
 var AudioContext = AudioContext || webkitAudioContext; // for ios/safari
 var context = new AudioContext();
-var activeNotes = [];
-var btnBox = document.getElementById('content'), btn = document.getElementsByClassName('button');
 var data, cmd, channel, type, note, velocity;
 
 // request MIDI access
@@ -18,14 +14,6 @@ else {
     alert("No MIDI support in your browser.");
 }
 
-// add event listeners
-document.addEventListener('keydown', keyController);
-document.addEventListener('keyup', keyController);
-for (var i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('mousedown', clickPlayOn);
-    btn[i].addEventListener('mouseup', clickPlayOff);
-}
-
 var sampleMap = {
     key60: 1,
     key61: 2,
@@ -33,65 +21,6 @@ var sampleMap = {
     key63: 4,
     key64: 5
 };
-// user interaction 
-function clickPlayOn(e) {
-    e.target.classList.add('active');
-    e.target.play();
-}
-
-function clickPlayOff(e) {
-    e.target.classList.remove('active');
-}
-
-function keyController(e) {
-    if (e.type == "keydown") {
-        switch (e.keyCode) {
-            case 81:
-                btn[0].classList.add('active');
-                btn[0].play();
-                break;
-            case 87:
-                btn[1].classList.add('active');
-                btn[1].play();
-                break;
-            case 69:
-                btn[2].classList.add('active');
-                btn[2].play();
-                break;
-            case 82:
-                btn[3].classList.add('active');
-                btn[3].play();
-                break;
-            case 84:
-                btn[4].classList.add('active');
-                btn[4].play();
-                break;
-            default:
-            //console.log(e);
-        }
-    }
-    else if (e.type == "keyup") {
-        switch (e.keyCode) {
-            case 81:
-                btn[0].classList.remove('active');
-                break;
-            case 87:
-                btn[1].classList.remove('active');
-                break;
-            case 69:
-                btn[2].classList.remove('active');
-                break;
-            case 82:
-                btn[3].classList.remove('active');
-                break;
-            case 84:
-                btn[4].classList.remove('active');
-                break;
-            default:
-            //console.log(e.keyCode);
-        }
-    }
-}
 
 // Returns the first input found, null if none found. 
 function getInput(midiAccess) {
@@ -135,7 +64,6 @@ function onMIDIMessage(event) {
     // pressure / tilt on
     // pressure: 176, cmd 11: 
     // bend: 224, cmd: 14
-    log('MIDI data', data);
     switch (type) {
         case 144: // noteOn message 
             noteOn(note, velocity);
@@ -145,8 +73,20 @@ function onMIDIMessage(event) {
             break;
     }
 			
-    //log('data', data, 'cmd', cmd, 'channel', channel);
-    logger(keyData, 'key data', data);
+    printKey(data);
+}
+
+// Prints one key to the console
+function printKey(keyData) {
+    
+    var message = "";
+    message += " channel = " + (keyData[0] & 0xf);
+    message += " cmd = " + (keyData[0] >> 4);
+    message += " type = " + (keyData[0] & 0xf0);
+    message += " note = " + keyData[1];
+    message += " velocity = " + keyData[2];
+    
+    console.log(message);
 }
 
 function onStateChange(event) {
@@ -155,13 +95,6 @@ function onStateChange(event) {
     if (type == "input")
         log("name", name, "port", port, "state", state);
 
-}
-
-function listInputs(inputs) {
-    var input = inputs.value;
-				log("Input port : [ type:'" + input.type + "' id: '" + input.id +
-        "' manufacturer: '" + input.manufacturer + "' name: '" + input.name +
-        "' version: '" + input.version + "']");
 }
 
 function noteOn(midiNote, velocity) {
@@ -185,7 +118,7 @@ function player(note, velocity) {
 }
 
 function onMIDIFailure(e) {
-    log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
+    console.error("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
 }
 
 // MIDI utility functions
@@ -197,10 +130,4 @@ function printInputDevice(midiAccess) {
     console.log("State " + input.state);
     console.log("Manufacturer " + input.manufacturer);
     console.log("Version " + input.version);
-}
-
-// utility functions
-function logger(container, label, data) {
-    messages = label + " [channel: " + (data[0] & 0xf) + ", cmd: " + (data[0] >> 4) + ", type: " + (data[0] & 0xf0) + " , note: " + data[1] + " , velocity: " + data[2] + "]";
-    container.textContent = messages;
 }
